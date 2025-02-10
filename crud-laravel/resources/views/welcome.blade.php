a1034f5fbc2fdb462101be6cc2ed4c8834bc4847
Molotto <molotto.luis@gmail.com>
2025-02-10 14:25:41 -0300

    implementando buscas

--- crud-laravel/resources/views/welcome.blade.php
+++ crud-laravel/resources/views/welcome.blade.php
@@ -4,14 +4,19 @@
 
 <div id="search-container" class="col-md-12">
        <h1>Busque um evento</h1>
-       <form action="">
+       <form action="/" method="GET">
               <input type="text" id="search" name="search" class="form-control" placeholder="Procurar um evento">
        </form>
 </div>
 
 <div id="events-container" class="col-md-12">
+       @if($search)
+       <h2>Buscando por: {{ $search }}</h2>
+
+       @else
        <h2>Próximos eventos</h2>
        <p class="subtitle">Veja os eventos dos próximos dias</p>
+       @endif
        <div id="cards-container" class="row">
               @foreach($events as $event)
                      <div class="card col-md-3">
@@ -24,8 +29,10 @@
                             </div>
                      </div>
               @endforeach
-              @if (count($events) == 0)
-                     <h3 style="text-decoration: underline red;">Não há eventos disponíveis</h3>
+              @if (count($events) == 0 && $search)
+                     <h3>Não foi possível encontrar nenhum evento com '{{ $search }}'! <br> <a href="/">Ver todos</a></h3>
+              @elseif(count($events) == 0)
+                     <h3>Não há eventos disponíveis!</h3>
               @endif
        </div>
 </div>
